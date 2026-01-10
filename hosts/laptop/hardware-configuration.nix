51fe44b4a341fd9ea70d60a0de67b116afded3a5
AsustelGames <alratna2027@gmail.com>
2026-01-10 22:16:53 +0100

    Updated hardware-configuration.nix

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -1,1 +1,2 @@
-generate hardware config for your hardware this is a useless file only needed for installing nixos using the NixOS_Install_Cheatsheet.txt
+generate hardware config for your hardware this is a useless file only needed for installing nixos using the NixOS_Install_Cheatsheet.txt because
+otherwise it won't install
